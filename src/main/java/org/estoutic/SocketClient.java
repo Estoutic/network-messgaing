15a6fbac9e1e529fc948a88b75cd19fc81eeed13
Estoutic <ilya_udalov_04@mail.ru>
2026-02-27 00:02:40 +0300

    add messaging

--- src/main/java/org/estoutic/SocketClient.java
+++ src/main/java/org/estoutic/SocketClient.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class SocketClient {
     public static void main(String[] args) {
@@ -12,9 +13,13 @@ public class SocketClient {
                 Socket socket = new Socket("localhost",8081);
                 DataInputStream inputStream = new DataInputStream(socket.getInputStream());
                 DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                Scanner scanner = new Scanner(System.in);
         ) {
-            outputStream.writeUTF("Hello from client");
-            System.out.println(inputStream.readUTF());
+            while (scanner.hasNextLine()) {
+                outputStream.writeUTF(scanner.nextLine());
+                System.out.println(inputStream.readUTF());
+
+            }
 
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
--- src/main/java/org/estoutic/SocketServer.java
+++ src/main/java/org/estoutic/SocketServer.java
@@ -3,6 +3,7 @@ package org.estoutic;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketServer {
 
@@ -10,10 +11,16 @@ public class SocketServer {
         try (ServerSocket serverSocket = new ServerSocket(8081);
              Socket socket = serverSocket.accept();
              DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());) {
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+             Scanner scanner = new Scanner(System.in);
 
-            System.out.println(inputStream.readUTF());
-            outputStream.writeUTF("Hello from server");
+             ) {
+            String request = inputStream.readUTF();
+            while (!request.equals("exit")) {
+                System.out.println(request);
+                outputStream.writeUTF(scanner.nextLine());
+                request = inputStream.readUTF();
+            }
 
 
         } catch (IOException e) {
